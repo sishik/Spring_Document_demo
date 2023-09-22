d45b94f23948e995e4b95c98e687380315c19cff
Shiva <singhshivakumar13@gmail.com>
2023-09-22 19:35:54 +0530

    Spring Doc-Building RESTful webservice commit-1

---
+++ src/main/java/com/example/spring_socument_demo/Greeting.java
@@ -0,0 +1,2 @@
+package com.example.spring_socument_demo;public class Greeting {
+}
---
+++ src/main/java/com/example/spring_socument_demo/SpringSocumentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_socument_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocumentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocumentDemoApplication.class, args);
+	}
+
+}
